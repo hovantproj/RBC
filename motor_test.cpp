ca39beded5f1ea4606d48a98bfdf62e689321eee
stereomaple <dwon0051@student.monash.edu>
2026-08-22 11:12:34 +1000

    Motor test code + plan

---
+++ motor_test.cpp
@@ -0,0 +1,35 @@
+// Motor controls for Motor A1 and A2 (wired together)
+const int in1 = 7; // in1 pin
+const int in2 = 8; // in2 pin
+
+// Motor controls for Motor B1 and B2 (wired together)
+const int in3 = 9; // in3 pin
+const int in4 = 10; // in4 pin
+
+void setup() {
+    // Set all motor control pins to output
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+}
+
+void loop() {
+    // Drive forward
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    delay(1000);
+
+    // Drive backwards
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    delay(1000);
+
+    // Turn one way
+
+    // Turn the other way
+}
---
+++ plan.md
@@ -0,0 +1,20 @@
+# Plan
+
+How do we assemble motors??
+- stalked other teams and figured it out from the picture (we were just not organising our parts)
+
+Organised parts
+
+Put motors on; need to test the following:
+- [ ] L298N motor module works as expected (along with code)
+- [ ] setup works - drives straight, might need to fix wobbling?
+
+Plan out the gripper design
+- the animals are really small and are light as
+- interlocking arms, just reach for everything in front and pull it in and secure it?
+- [ ] will need to test that they properly secure animals
+
+Figure out how everything will sit together...
+- [ ] figure out how to mount the gripper onto the robot
+- [ ] decide where the arduino will sit (powered by L298N module?)
+- [ ] also need a space for the 
