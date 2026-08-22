b43e25502ff7e265ed0baa68dfe5b780e4b0b0dd
stereomaple <dwon0051@student.monash.edu>
2026-08-22 11:56:31 +1000

    Edited motor test code for flashing LED

--- motor_test.cpp
+++ motor_test.cpp
@@ -1,3 +1,5 @@
+#define LED_PIN 13
+
 // Motor controls for Motor A1 and A2 (wired together)
 const int in1 = 7; // in1 pin
 const int in2 = 8; // in2 pin
@@ -7,6 +9,9 @@ const int in3 = 9; // in3 pin
 const int in4 = 10; // in4 pin
 
 void setup() {
+    // initialize digital pin LED_PIN as an output for testing.
+    pinMode(LED_PIN, OUTPUT);
+
     // Set all motor control pins to output
     pinMode(in1, OUTPUT);
     pinMode(in2, OUTPUT);
@@ -15,12 +20,16 @@ void setup() {
 }
 
 void loop() {
+    digitalWrite(LED_PIN , HIGH);
+
     // Drive forward
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
     digitalWrite(in3, HIGH);
     digitalWrite(in4, LOW);
     delay(1000);
+
+    digitalWrite(LED_PIN , LOW);
 
     // Drive backwards
     digitalWrite(in1, LOW);
